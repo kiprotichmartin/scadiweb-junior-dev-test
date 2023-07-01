b8b08673db44d5be2a7be6f8dac4d457cdba3d37
Martin Kiprotich <martinkiprotich.mk@gmail.com>
2023-07-01 12:26:28 +0300

    Autoload classes automatically and display msg on 'Home' page

--- Server/public/index.php
+++ Server/public/index.php
@@ -16,7 +16,8 @@ spl_autoload_register(function ($class) {
   $root = dirname(__DIR__); // get the parent directory
   $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
   if (is_readable($file)) {
-    require $root . '/' . str_replace('\\', '/', $class) . '.php';
+    // require $root . '/' . str_replace('\\', '/', $class) . '.php';
+    require $file;
   }
 });
 
