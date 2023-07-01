9d89170545329b3794df7d0ca657294aa6ee9b74
Martin Kiprotich <martinkiprotich.mk@gmail.com>
2023-07-01 12:03:47 +0300

    Autoload classes automatically and display msg on 'Home' page

---
+++ Server/App/Controllers/Home.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+/**
+ * Home controller
+ *
+ * PHP version 8.2.4
+ */
+class Home
+{
+
+    /**
+     * Show the index page
+     *
+     * @return void
+     */
+    public function index()
+    {
+        echo 'Hello from the index action in the Home controller!';
+    }
+}
