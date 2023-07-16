315735ff41082d5ca3c5b4a3482518beeaef9d59
Martin Kiprotich <martinkiprotich.mk@gmail.com>
2023-07-16 22:03:00 +0300

    Replace the autoload function in (public/index.php) with the Composer autoloader

--- Server/public/index.php
+++ Server/public/index.php
@@ -7,7 +7,7 @@
  */
 
 /**
- * Composer
+ * Composer autoloader
  */
 require '../vendor/autoload.php';
 
@@ -15,14 +15,14 @@ require '../vendor/autoload.php';
 /**
  * Autoloader
  */
-spl_autoload_register(function ($class) {
-  $root = dirname(__DIR__); // get the parent directory
-  $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
-  if (is_readable($file)) {
-    // require $root . '/' . str_replace('\\', '/', $class) . '.php';
-    require $file;
-  }
-});
+// spl_autoload_register(function ($class) {
+//   $root = dirname(__DIR__); // get the parent directory
+//   $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
+//   if (is_readable($file)) {
+//     // require $root . '/' . str_replace('\\', '/', $class) . '.php';
+//     require $file;
+//   }
+// });
 
 
 /**
