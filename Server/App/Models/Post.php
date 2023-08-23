f14411b1b8957e92517368bb35ac2a56c658ae9e
Martin Kiprotich <martinkiprotich.mk@gmail.com>
2023-08-23 11:03:46 +0300

    Add a model - get data from the database and display it in a view

---
+++ Server/App/Models/Post.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use PDO;
+
+/**
+ * Post model
+ *
+ * PHP version 8.2.4
+ */
+class Post
+{
+
+    /**
+     * Get all the posts as an associative array
+     *
+     * @return array
+     */
+    public static function getAll()
+    {
+        $host = 'localhost';
+        $dbname = 'mvc';
+        $username = 'root';
+        $password = '';
+
+        try {
+            $db = new PDO(
+                "mysql:host=$host;dbname=$dbname;charset=utf8",
+                $username,
+                $password
+            );
+
+            $stmt = $db->query('SELECT id, title, content FROM posts
+                                ORDER BY created_at');
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            return $results;
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+}
