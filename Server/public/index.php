d9f7f2181bcef78eb215911a496928ce18dd9bc5
Martin Kiprotich <martinkiprotich.mk@gmail.com>
2023-10-09 20:52:08 +0300

    Configured [error_reporting()] function to display all errors

--- Server/public/index.php
+++ Server/public/index.php
@@ -15,6 +15,7 @@ require '../vendor/autoload.php';
 /**
  * Error and Exception handling
  */
+error_reporting(E_ALL);
 set_error_handler('Core\Error::errorHandler');
 set_exception_handler('Core\Error::exceptionHandler');
 
